0f9cdef2306fdcd5543c13c40c07af179df53fbc
DHRUV <makotoisamu16@gmail.com>
2023-10-16 19:38:14 +0530

    CREATED TO-DO LIST APP

---
+++ TO_DO_LIST/script.js
@@ -0,0 +1,60 @@
+let arr = getItemFromLocalStorage();
+
+display();
+
+function onEnterPress(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+}
+
+function addTask(){
+    let inputEl = document.getElementById('EnterTaskInput').value;
+    if (inputEl){
+        arr.push(inputEl);
+        saveToLocalStorage(arr);
+        document.getElementById('EnterTaskInput').value ='';
+    }
+    display();
+}
+
+function onEnterPress(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+}
+
+function display(){
+    let container = document.querySelector('.todoContainer')
+    let newHTML = ' '
+    for (let i = 0; i<arr.length; i++){
+        newHTML+=`
+        <div class="script_container">
+            <span class="todo_tasks">${arr[i]}</span>
+            <button class="todo_tasks_button" 
+            onclick="deleteItems();"><i class="ri-delete-bin-fill"></i></button>
+        </div>`
+
+    }
+    container.innerHTML= newHTML
+}
+
+function deleteItems(){
+    arr.pop();
+    saveToLocalStorage(arr);
+    display();
+}
+
+function getItemFromLocalStorage(){
+    let savedItem = localStorage.getItem('Tasks');
+    if(savedItem){
+        return JSON.parse(savedItem)
+    } else {
+        return [];
+    }
+
+}
+
+function saveToLocalStorage(tasks){
+    localStorage.setItem('Tasks', JSON.stringify(tasks));
+}
